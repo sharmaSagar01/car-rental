db868120ff0e2f0d46fc380f0eae4a816e7b007a
sharmaSagar01 <sagaarpkr@gmail.com>
2023-07-30 16:07:20 -0400

    Added Search Bar for Filtering Manufaturer

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,15 @@ import Hero from "./Hero";
 import CustomeButton from "./CustomeButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-export { Hero, CustomeButton, Navbar, Footer };
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+export {
+  Hero,
+  CustomeButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
